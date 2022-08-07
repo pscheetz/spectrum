47d650e17faa4656275a9d8ef12d958486b6dacf
Philip Scheetz <philip@scheetznet.com>
2022-08-06 17:44:26 -0700

    Added fancier output and made bug fixes

--- rock-paper-scissors/game-final.py
+++ rock-paper-scissors/game-final.py
@@ -2,39 +2,63 @@ from os import system, name
 import os 
 import random
 
+# Declaring global variables
 game_type = ''
 games_played = 0
 users_name = os.environ.get('USER', os.environ.get('USERNAME'))
-"""
-Score works like this:
-x: "game type", "score": {"user": <int>, "cpu":<int>, "tie":<int>} # Dict of the score
-"""
-final_score = []
+
+# Score works like this:
+# x: "game type", "score": {"user": <int>, "cpu":<int>, "tie":<int>} # Dict of the score
+final_score = [] # This is a list to be filled with a dict later (see note above)
+
 
 def print_tally():
     global final_score
+    player_win_tally = 0
+    cpu_win_tally = 0
+    tie_tally = 0
     print("Final_score")
     clear()
     if not final_score: # Don't show it if it's empty
         print("There is no scoreboard yet. Go play a game first, then come back!")
     else:
-        print("Here is the scoreboard.")
-        print(final_score)
-        # OUTPUT LOOKS LIKE THIS
-        # [{1: 'RPS', 'score': {'user': 1, 'cpu': 1, 'tie': 1}},
-        # {2: 'RPS', 'score': {'user': 2, 'cpu': 4, 'tie': 8}}]
-        print("Your Score: %i | CPU Score: %i | Ties: %i | Type 'quit' to exit"%(score_tally['user'],score_tally['cpu'],score_tally['tie']))
-        print("Games Played: %s"%final_score.len())
+        # print("Here is the raw scoreboard.")
+        # print(final_score)
         
-
+        print("\nGames Played: %s"%len(final_score))
+        
+        # Figure out how many games won
+        current_game = 0
+        for i in final_score:
+            current_game+=1
+            game_played = i[current_game]
+            player_score = i["score"]["user"] # Gets the current game's scoreboard, and the user key
+            cpu_score = i["score"]["cpu"]
+            tie_score = i["score"]["tie"]
+            
+            print("Game %s — %s. Player Score: %s | CPU Score: %s | Ties: %s"%(current_game, game_played, player_score, cpu_score, tie_score))
+            
+            if player_score > cpu_score:
+                winner = "Player"
+                player_win_tally = player_win_tally + 1
+            elif cpu_score > player_score:
+                winner = "CPU"
+                cpu_win_tally = cpu_win_tally + 1
+            else:
+                winner = "tie"
+                tie_tally = tie_tally + 1
+        
+        # print("The winner is %s"%winner)
+        print("\nOverall Scores\n---------------")
+        print("Player won: %s"%player_win_tally)
+        print("CPU won: %s"%cpu_win_tally)
+        print("Ties: %s"%tie_tally)
 
     input("\nPress the Enter key to continue...")
 
 
 def add_tally(scoreboard): # This adds the final games core to the overall scoreboard of games
-    global games_played
-    global final_score
-    global game_type
+    global games_played, final_score, game_type
     
     final_score.append({games_played:game_type,"score":scoreboard});
     
@@ -47,8 +71,7 @@ def clear():
         os.system("clear")
 
 def rps(): # Plays Rock Paper Scissors
-    global games_played
-    global game_type
+    global games_played, game_type
     games_played += 1 # advances global games played
     game_type = "RPS"
 
@@ -116,9 +139,10 @@ def rps(): # Plays Rock Paper Scissors
 
     
 def rpsls():
-    game = "rpsls"
+    global game_type
+    game_type = "R.P.S.L.S."
     print("RPSLS called")
-    print(game)
+    print(game_type)
     
 
 
@@ -138,7 +162,8 @@ while True:
         rps()
         
     elif choice == "2":
-        rpsls()
+        rps() # placeholder for now 
+        #rpsls()
     elif choice == "3":
         print_tally()
     elif choice == "quit" or choice == "exit" or choice == "4":
